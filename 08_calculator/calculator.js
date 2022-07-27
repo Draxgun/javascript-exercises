5de7c68feb4db9423bfc0d2c91d5face8ddf26bb
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-27 14:28:46 -0600

    calculator excercise finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+  for(let i=0;i<array.length;i++){
+    sum+= array[i]
+  }
+  console.log(sum)
+  return sum
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  let total = 1
+  for(let i=0;i<array.length;i++){
+    total= total*array[i]
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = a;
+  if(a!=0){
+    for(let i=1;i<=a-1;i++){
+      console.log(i);
+      result = result*i;
+    }
+    return result;
+  }else{
+    return 1;
+  }
+
 };
 
 // Do not edit below this line
