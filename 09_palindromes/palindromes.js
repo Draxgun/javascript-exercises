1a65066d189c1874522bc2702aa1eb813e729c40
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-27 22:20:45 -0600

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    str = str.replace(/[^a-zA-Z]/g,"");
+    let splitString = str.split("");
+    let reverseStrArray = splitString.reverse();
+    let reverseStr = reverseStrArray.join("");
+    if (str===reverseStr) 
+    {
+        return true 
+    }else{
+        return false
+    }
 };
+
+
+console.log(palindromes('A car, a man, a maraca'))
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
