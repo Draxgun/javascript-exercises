591ea0fb26a8e1d43015a92717a5f73c72a73f82
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-28 10:22:11 -0600

    excercies done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array){
+    return array.map(a => a.title)
 };
+
+
+  
 
 // Do not edit below this line
 module.exports = getTheTitles;
